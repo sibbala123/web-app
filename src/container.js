a52109b13420bbc93498ddea0f84adfc2dd0ca9b
Sibbala JAYANTH <sibbala.jayanth@amadeus.com>
2022-04-07 09:56:06 +0530

    second

--- src/container.js
+++ src/container.js
@@ -9,22 +9,25 @@ const Layout = () => {
     const classes = useStyles();
   
   
-    const clear = () => {
-      
-      setPostData({ email:'',selectedFile: '' });
-    };
   
-    const handleSubmit = async (e) => {
-      e.preventDefault();
+    const handleSubmit = async (postData) => {
+      console.log("function called")
+      if (!(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(postData.email)))
+  {
+    alert("You have entered an invalid email address!")
+    setPostData({email:""})
+  }
+    
+      console.log(postData);
   
     };
   
     return (
       <Paper className={classes.paper}>
-        <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+        <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={(e)=>handleSubmit(postData)}>
           <div className={classes.header}><Typography variant="h5">DNB SEARCH AUTOMATION TOOL</Typography></div>
           <div className={classes.flexLayout}>
-              <div sm="4" style={{ fontWeight: 400 }}>Upload the file for DNB search</div><input id="fileSelect" type="file" accept=".csv" /><Template /> </div>
+              <div sm="4" style={{ fontWeight: 400 }}>Upload the file for DNB search</div><input id="fileSelect" type="file" value={postData.selectedFile} onChange={(e) => setPostData({ ...postData, selectedFile: e.target.value })} accept=".csv" /><Template /> </div>
               
           <div className={classes.flexLayout}><div style={{ fontWeight: 400 }}>Enter email to export output file through mail</div><TextField name="email" variant="outlined" label="Email" fullWidth value={postData.email} onChange={(e) => setPostData({ ...postData, email: e.target.value })} /></div>
           <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" >Submit</Button>
